5a55b11041215461c83700c15ecb9cd6442fcfbb
Savio Tsui <stsui@splunk.com>
2020-01-03 06:50:31 +0000

    ci(*): skip all confirmation steps in publish.sh

--- cd/publish.sh
+++ cd/publish.sh
@@ -7,7 +7,7 @@ echo "Building package ..."
 yarn build
 
 echo "Publishing latest versions of updated packages to artifactory ..."
-yarn lerna publish from-package --no-git-reset --dist-tag latest
+yarn lerna publish from-package --no-git-reset --dist-tag latest --yes
 
 # echo "Publishing docs to artifactory ..."
 # yarn publish:docs

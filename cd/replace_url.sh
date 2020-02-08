caf3a31b864c0273cf3fcb826075b29590468e26
Savio Tsui <stsui@splunk.com>
2020-02-07 20:22:10 -0800

    ci(*): exclude lock files from being modified in replace_url

--- cd/replace_url.sh
+++ cd/replace_url.sh
@@ -1,2 +1,2 @@
 echo "Replacing project urls in repo... (CHANGELOG, README, etc.)"
-yarn replace $CI_PROJECT_URL $GITHUB_PROJECT_URL * -r --include="*.md,*.txt,*.ts,*.json" --exclude="node_modules/*"
+yarn replace $CI_PROJECT_URL $GITHUB_PROJECT_URL * -r --include="*.md,*.txt,*.ts,*.json" --exclude="node_modules/*,*/package-lock.json,*.lock"
